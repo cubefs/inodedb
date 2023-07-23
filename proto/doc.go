9b9dd69b47df781e5c4799f8c202778274191642
Haifeng Liu <bladehliu@users.noreply.github.com>
2023-07-23 22:06:50 +0800

    kick off

---
+++ proto/doc.go
@@ -0,0 +1,25 @@
+/* 
+
+For Master:
+
+CreateNamespace
+
+AddInodeserver
+
+
+For Inodeserver: 
+
+CreateInodeRange(rangeNumber, startIno, endIno)
+
+NewInode
+
+UpdateInode
+
+RemoveInode
+
+LinkInode(parent, child, name, timestamp)
+
+Search
+
+*/
+package proto
